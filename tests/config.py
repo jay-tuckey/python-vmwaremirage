3d06a883e1e3b111f30702671af9aa8e151327a8
Jay Tuckey <jay.tuckey@gmail.com>
2017-09-19 19:56:04 +0930

    Added some test config

---
+++ tests/config.py
@@ -0,0 +1,24 @@
+# Test server
+server = ''
+username = ''
+
+# Example CVD's
+cvd_1 = {
+    'name': 'LR90L018D',
+    'id': 10025,
+    'deviceid': 25,
+    'policyid': 4,
+    'username': '',
+    'policyname': 'Staff Machines',
+    'progress': 100
+}
+
+cvd_2 = {
+    'name': 'LR90L1DTH',
+    'id': 10031,
+    'deviceid': 31,
+    'policyid': 4,
+    'username': '',
+    'policyname': 'Staff Machines',
+    'progress': 100
+}
